686817f6e94865e051f846d6694d04ec8bd50c60
zhaokai <582533985@qq.com>
2020-08-28 10:09:41 +0800

    增加了 AES128加密，RSA加密

---
+++ src/main/java/com/example/ttt/AES128.java
@@ -0,0 +1,160 @@
+package com.example.ttt;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class AES128 {
+    /**
+     * 加密
+     * @param sSrc
+     * @param sKey
+     * @return
+     * @throws Exception
+     */
+    public static String Encrypt(String sSrc, String sKey) throws Exception {
+        if (sKey == null) {
+            System.out.print("Key为空null");
+            return null;
+        }
+        // 判断Key是否为16位
+        if (sKey.length() != 16) {
+            System.out.print("Key长度不是16位");
+            return null;
+        }
+        byte[] raw = sKey.getBytes("utf-8");
+        SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+        Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");//"算法/模式/补码方式"
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
+        byte[] encrypted = cipher.doFinal(sSrc.getBytes("utf-8"));
+
+        return new Base64().encodeToString(encrypted);//此处使用BASE64做转码功能，同时能起到2次加密的作用。
+    }
+
+    /**
+     * 解密
+     * @param sSrc
+     * @param sKey
+     * @return
+     * @throws Exception
+     */
+    public static String Decrypt(String sSrc, String sKey) throws Exception {
+        try {
+            // 判断Key是否正确
+            if (sKey == null) {
+                System.out.print("Key为空null");
+                return null;
+            }
+            // 判断Key是否为16位
+            if (sKey.length() != 16) {
+                System.out.print("Key长度不是16位");
+                return null;
+            }
+            byte[] raw = sKey.getBytes("utf-8");
+            SecretKeySpec skeySpec = new SecretKeySpec(raw, "AES");
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
+            byte[] encrypted1 = new Base64().decode(sSrc);//先用base64解密
+            try {
+                byte[] original = cipher.doFinal(encrypted1);
+                String originalString = new String(original,"utf-8");
+                return originalString;
+            } catch (Exception e) {
+                System.out.println(e.toString());
+                return null;
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+            return null;
+        }
+    }
+
+    /**
+     * 生成密钥（盐）
+     * @param n 密钥长度
+     * @return
+     */
+    public static String getCKey(int n) {
+        String val = "";
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            String str = random.nextInt(2) % 2 == 0 ? "num" : "char";
+            if ("char".equalsIgnoreCase(str)) { // 产生字母
+                int nextInt = random.nextInt(2) % 2 == 0 ? 65 : 97;
+                // System.out.println(nextInt + "!!!!"); 1,0,1,1,1,0,0
+                val += (char) (nextInt + random.nextInt(26));
+            } else if ("num".equalsIgnoreCase(str)) { // 产生数字
+                val += String.valueOf(random.nextInt(10));
+            }
+        }
+        return val;
+    }
+
+    public static void main(String[] args) throws Exception {
+//        /*
+//         * 此处使用AES-128-ECB加密模式，key需要为16位。
+//         */
+        //String cKey = "1234567890123h56";
+        String cKey = getCKey(16);
+        System.out.println("密钥："+cKey);
+        // 需要加密的字串
+        String cSrc = "zhaokai";
+        System.out.println(cSrc);
+//        // 加密
+//        String enString = AES128.Encrypt(cSrc, cKey);
+//        System.out.println("加密后的字串是：" + enString);
+//
+//        // 解密
+//        String DeString = AES128.Decrypt(enString, cKey);
+//        System.out.println("解密后的字串是：" + DeString);
+        AES128.encrypt(cKey,cSrc);
+    }
+
+    /**
+     * 加密
+     *
+     * @baiparam content 需要加密的内容du
+     * @param password 加密密码
+     * @return
+     */
+    public static byte[] encrypt(String content, String password) {
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(password.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");// 创建密zhi码器dao
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE, key);// 初始化
+            byte[] result = cipher.doFinal(byteContent);
+            System.out.println(result.length);
+            for (byte a:result
+                 ) {
+                System.out.print(a+" ");
+            }
+            return result; // 加密
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }  catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
